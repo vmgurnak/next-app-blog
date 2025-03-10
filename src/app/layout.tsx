630c283bc3ca3cd6eca68c9c665e9d7f89f40c4f
Valeriy.Gurnak <vmgurnak@gmail.com>
2025-03-10 11:42:23 +0200

    css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
-import './globals.css';
+
+import './styles/globals.css';
+import 'modern-normalize/modern-normalize.css';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
